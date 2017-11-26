200a69e4a37a40942fdd77cdb1bc6c0128ede3ec
Nicklas Persson <gorlingy@hotmail.com>
2017-11-26 07:12:18 +0100

    Test av photosensor och temperatur

--- IOTgarden.ino
+++ IOTgarden.ino
@@ -1,22 +1,25 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
 #include <SoftwareSerial.h>
-#include <stdlib.h>
-#define DEBUG true
-#define DHTPIN 7                 
+#define ONE_WIRE_BUS 10               
 #define SSID "NETGEAR74"     // "SSID-WiFiname"
 #define PASS "XX" // "password"
 #define IP "api.thingspeak.com"      // thingspeak.com ip
 
 String msg = "GET /update?key=XX"; //Your ThingSpeak API key
+//Instances
 SoftwareSerial esp8266(2,3); //Rx,Tx
-unsigned long lastTimeMillis = 0;
-
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 //Variables
+unsigned long lastTimeMillis = 0;
 int chk;
 float temp;
 int hum;
-int sensorValue;
-int sensorValue1;
-int sensorValue2;
+int mSensorValue;
+int mSensorValue1;
+int mSensorValue2;
+float Celcius;
 int pResistor;
 int error;
 String count; 
@@ -25,13 +28,14 @@ void setup()
   Serial.begin(9600);
   esp8266.begin(9600);
   delay(100);
-
+  sensors.begin();
   Serial.println("AT");
   esp8266.println("AT");
-
+  printResponse();
   delay(1000);
+  pinMode(A5,INPUT);
  
-  connectWiFi();
+ // connectWiFi();
   
   delay(1000);
 }
@@ -39,28 +43,33 @@ void setup()
 void loop()
 {
 
-  //lcd.autoscroll(); 
- // start: //label
-   // error=0;
-   // sensorValue = analogRead(A0);
-   // sensorValue1 = analogRead(A1);
-   // sensorValue2 = analogRead(A2);
+  start: //label
+    error=0;
+  //  mSensorValue = analogRead(A0);
+  //  mSensorValue1 = analogRead(A1);
+  //  mSensorValue2 = analogRead(A2);
+	pResistor = analogRead(A5);
+    sensors.requestTemperatures();
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+	Celcius = sensors.getTempCByIndex(0) - 2.49;
+	Serial.println(Celcius);
 
-
-   // Serial.println("M= ");
-   // Serial.println(sensorValue);
-  //  delay(1000);
-	  Getinfo();
-   // updateTemp();
-    //Resend if transmission is not completed
-  // if (error==1)
-    //{
-     // goto start; //go to label "start"
-   // } 
+	Serial.println("P= ");
+	Serial.println(pResistor);
+    Serial.println("M= ");
+    Serial.println(mSensorValue);
+    delay(1000);
+    //  Getinfo();
+    //updateTemp();
+   // Resend if transmission is not completed
+   if (error==1)
+    {
+      goto start; //go to label "start"
+    } 
  // delay(36000);
 }
 
-
+// POST SENSORVALUE
 void updateTemp()
 {
   String cmd = "AT+CIPSTART=\"TCP\",\"";
@@ -70,18 +79,16 @@ void updateTemp()
   esp8266.println(cmd);
   cmd = msg;
   cmd += "&field1=";    //field 1 for temperature
-  cmd += sensorValue;
+  cmd += mSensorValue;
   cmd += "&field2=";    //field 1 for temperature
-  cmd += sensorValue1;
+  cmd += mSensorValue1;
   cmd += "&field3=";    //field 1 for temperature
-  cmd += sensorValue2;
+  cmd += mSensorValue2;
   cmd += "\r\n";
   delay(200);
   count = cmd.length();
   Serial.println("AT+CIPSEND="+count);
   esp8266.println("AT+CIPSEND="+count);
-  //Serial.println(cmd.length());
-  //esp8266.println(cmd.length());
   delay(5000);
 
    
@@ -132,17 +139,17 @@ boolean connectWiFi()
 {
   Serial.println("AT+CWMODE=3");
   esp8266.println("AT+CWMODE=3");
-
+  printResponse();
   delay(2000);
   String cmd="AT+CWJAP=\"";
-
+  printResponse();
   cmd+=SSID;
   cmd+="\",\"";
   cmd+=PASS;
   cmd+="\"";
   Serial.println(cmd);
   esp8266.println(cmd);
-
+  printResponse();
   delay(5000);
   
     return true;
